4345ce6f160a186f96f70d63460aa009beeef80a
kevinseabourne <kevinseabourne@hotmail.com>
2021-08-29 20:09:44 +0800

    updated header

--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,39 @@ import styled from "styled-components";
 import Link from "next/link";
 import ImageLoader from "./reusable/imageLoader";
 import Logo from "../public/icons/audiophile.svg";
+import Cart from "./cart";
 
-interface Props {}
+interface Props {
+  cartItems: {
+    id: string;
+    date: string;
+    images: {
+      image: string;
+      title: string;
+    }[];
+    inTheBox: {
+      units: string;
+      title: string;
+    };
+    longDescription: string;
+    price: string;
+    shortDescription: string;
+    title: string;
+    type: string;
+    cartQuantity: number;
+  }[];
+  handleCartItemQuantityChange: (
+    operation: "decrease" | "increase",
+    id: string
+  ) => void;
+  cartSubTotalPrice: string;
+}
 
-const Header: React.FC<Props> = () => {
+const Header: React.FC<Props> = ({
+  cartItems,
+  handleCartItemQuantityChange,
+  cartSubTotalPrice,
+}) => {
   const [links] = useState([
     { title: "Home", route: "/" },
     {
@@ -34,11 +63,17 @@ const Header: React.FC<Props> = () => {
       />
       <LinksContainer>
         {links.map((link) => (
-          <Link href={link.route}>
-            <LinkTitle key={link.title}>{link.title}</LinkTitle>
+          <Link href={link.route} key={link.title}>
+            <LinkTitle>{link.title}</LinkTitle>
           </Link>
         ))}
       </LinksContainer>
+
+      <Cart
+        cartItems={cartItems}
+        handleCartItemQuantityChange={handleCartItemQuantityChange}
+        cartSubTotalPrice={cartSubTotalPrice}
+      />
     </Container>
   );
 };
@@ -46,14 +81,21 @@ const Header: React.FC<Props> = () => {
 export default Header;
 
 const Container = styled.div`
+  max-width: 1109.83px;
   width: 100%;
   height: 93px;
   position: fixed;
   top: 0px;
   left: 0px;
+  bottom: 0px;
+  right: 0px;
+  margin-left: auto;
+  margin-right: auto;
+  z-index: 20;
+  border-bottom: 1px solid white;
 `;
 
-const LinkTitle = styled.span``;
+const LinkTitle = styled.h4``;
 
 const LinksContainer = styled.div`
   display: flex;
