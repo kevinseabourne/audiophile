8363abbaefbffd6d5da46f5807496a82f3933427
kevinseabourne <kevinseabourne@hotmail.com>
2021-08-29 20:11:56 +0800

    handle cart functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import { ThemeProvider } from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import { AppProps } from "next/app";
 import { GlobalStyle } from "../globalStyle";
 import Header from ".././components/header";
@@ -28,15 +29,57 @@ const theme: MyTheme = {
 };
 
 function App({ Component, pageProps }: AppProps) {
+  const [cartItems, setCartItems] = useState([]);
+  const [cartSubTotalPrice, setCartSubTotalPrice] = useState("");
+
+  const handleCartItemQuantityChange = (
+    operation: "increase" | "decrease",
+    id: string
+  ): void => {
+    const updatedCart = cartItems.map((cartItem) => {
+      if (cartItem.id === id) {
+        cartItem.cartQuantity =
+          operation === "increase"
+            ? cartItem.cartQuantity + 1
+            : cartItem.cartQuantity - 1;
+        return cartItem;
+      }
+      return cartItem;
+    });
+
+    handleCartSubTotal(updatedCart);
+
+    setCartItems(updatedCart);
+  };
+
+  const handleCartSubTotal = (updatedCart) => {};
+
   return (
     <>
-      <Header />
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Header
+          cartItems={cartItems}
+          handleCartItemQuantityChange={handleCartItemQuantityChange}
+          cartSubTotalPrice={cartSubTotalPrice}
+        />
+        <Wrapper>
+          <Component {...pageProps} />
+        </Wrapper>
       </ThemeProvider>
     </>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 93px;
+  z-index: -100;
+  max-width: 1109.83px;
+  background-color: white;
+`;
