ad109f0da98aa33a0a19b9d433dc07e5753fb773
kevinseabourne <kevinseabourne@hotmail.com>
2021-10-12 21:09:05 +0800

    added checkout page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,11 +1,18 @@
 import styled from "styled-components";
+import Input from "../components/reusable/input";
 
 interface Props {}
 
 const Checkout: React.FC<Props> = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Form></Form>
+    </Container>
+  );
 };
 
 export default Checkout;
 
 const Container = styled.div``;
+
+const Form = styled.form``;
