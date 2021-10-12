69f918e7e83eb625c0f0e585870e38b3009d09d3
kevinseabourne <kevinseabourne@hotmail.com>
2021-10-12 21:28:53 +0800

    updated product page

--- pages/products/[category]/[product]/index.tsx
+++ pages/products/[category]/[product]/index.tsx
@@ -5,6 +5,7 @@ import ProductGallery from "../../../../components/productGallery";
 import RelatedProducts from "../../../../components/relatedProducts";
 import CategoryImageLinks from "../../../../components/categoryImageLinks";
 import AboutUs from "../../../../components/aboutUs";
+import ProductCard from "../../../../components/productCard";
 
 interface Props {}
 
@@ -13,7 +14,8 @@ const ProductPage: React.FC<Props> = () => {
   return (
     <Container>
       <Return>Go Back</Return>
-      <ItemContainer>image, price, little info</ItemContainer>
+
+      <ProductCard />
       <MoreProductInfo>more product info</MoreProductInfo>
       <ProductGallery />
       <RelatedProducts />
@@ -25,11 +27,16 @@ const ProductPage: React.FC<Props> = () => {
 
 export default ProductPage;
 
-const Container = styled.div`
-  background-color: red;
-`;
+const Container = styled.div``;
 
-const Return = styled.div``;
+const Return = styled.div`
+  margin-top: 79px;
+  margin-bottom: 56px;
+  margin-right: auto;
+  font-weight: 200;
+  opacity: 0.5;
+  color: ${({ theme }) => theme.colors.darkerBlack};
+`;
 
 const ItemContainer = styled.div``;
 
