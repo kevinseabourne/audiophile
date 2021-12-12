ea4054736255ea56ed5bba1f818530a93307ebbe
kevinseabourne <kevinseabourne@hotmail.com>
2021-12-12 19:09:58 +0800

    dynamic page

--- pages/products/[category]/index.tsx
+++ pages/products/[category]/index.tsx
@@ -1,5 +1,6 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import AppContext from "../../../context/appContext";
 import styled from "styled-components";
 import CategoryImageLinks from "../../../components/categoryImageLinks";
@@ -7,13 +8,40 @@ import AboutUs from "../../../components/aboutUs";
 import ImageLoader from "../../../components/reusable/imageLoader";
 import Button from "../../../components/reusable/button";
 import ProductCard from "../../../components/productCard";
+import { getCategoryProducts } from "../../api/products";
+import { isArrayEmpty } from "../../../lib/utils/isEmpty";
 
-interface Props {}
+interface Props {
+  data: {
+    productAdded: string;
+    id: string;
+    images: {
+      title: string;
+      image: string;
+    }[];
+    inTheBox: {
+      title: string;
+      units: number;
+    }[];
+    longDescription: string;
+    price: string;
+    shortDescription: string;
+    title: string;
+    lowerCaseTitle: string;
+    type: string;
+  }[];
+}
 
-const ProductsPage: React.FC<Props> = () => {
+const ProductsPage: React.FC<Props> = ({ data }) => {
   const { links } = useContext(AppContext);
   const { query } = useRouter();
   const { category } = query;
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    data && setProducts(data);
+  }, []);
 
   return (
     <Container>
@@ -22,7 +50,9 @@ const ProductsPage: React.FC<Props> = () => {
       </Banner>
 
       <ProductsContainer>
-        <ProductCard />
+        {products.map((product, index) => (
+          <ProductCard key={product.id} index={index + 1} product={product} />
+        ))}
       </ProductsContainer>
       <CategoryImageLinks links={links} />
       <AboutUs />
@@ -30,11 +60,39 @@ const ProductsPage: React.FC<Props> = () => {
   );
 };
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { category: "headphones" } },
+      { params: { category: "speakers" } },
+      { params: { category: "earphones" } },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { category } = params;
+  let data = [];
+  if (typeof category === "string") {
+    data = await getCategoryProducts(category);
+  }
+
+  if (!isArrayEmpty(data)) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      data,
+      params,
+    },
+  };
+};
+
 export default ProductsPage;
 
-const Container = styled.div`
-  ${"" /* background-color: red; */}
-`;
+const Container = styled.div``;
 
 const Banner = styled.div`
   position: absolute;
@@ -57,7 +115,7 @@ const ProductsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 1110px);
   grid-column-end: auto;
-  grid-gap: 24px 0px;
+  grid-gap: 160px 0px;
   max-width: 560px;
   margin-bottom: 160px;
 `;
